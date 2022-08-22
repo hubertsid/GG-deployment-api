1c3bd05f063483698facc5f51232e1a49179df8c
Hubert Sidorowicz <80327910+hubertsid@users.noreply.github.com>
2022-08-22 14:10:47 +0200

    test

--- api.py
+++ api.py
@@ -8,18 +8,18 @@ app = Flask(__name__)
 def home_view():
     return "<h1>Welcome to gamer grandma</h1>"
 
-@app.route('/api', methods=['POST'])
-def test_post():
-    request_data = request.get_json()
-    gm = []
+# @app.route('/api', methods=['POST'])
+# def test_post():
+#     request_data = request.get_json()
+#     gm = []
 
-    data=json.dumps(request_data)
-    collection = json.loads(data)
+#     data=json.dumps(request_data)
+#     collection = json.loads(data)
     
-    for i in collection['games']:
-        gm.append(i)
-    recs = algorithm.give_reccomendations(gm)
+#     for i in collection['games']:
+#         gm.append(i)
+#     recs = algorithm.give_reccomendations(gm)
 
-    return jsonify(game = recs)
+#     return jsonify(game = recs)
 
 app.run(port=3000)
