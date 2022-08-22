ddd936f687d6c9e58275402fe111f45af863eb4e
Hubert Sidorowicz <80327910+hubertsid@users.noreply.github.com>
2022-08-22 13:45:41 +0200

    push source code

---
+++ api.py
@@ -0,0 +1,21 @@
+import algorithm
+from flask import *
+import json
+
+app = Flask(__name__)
+
+@app.route('/test', methods=['POST'])
+def test_post():
+    request_data = request.get_json()
+    gm = []
+
+    data=json.dumps(request_data)
+    collection = json.loads(data)
+    
+    for i in collection['games']:
+        gm.append(i)
+    recs = algorithm.give_reccomendations(gm)
+
+    return jsonify(game = recs)
+
+app.run(port=3000)
