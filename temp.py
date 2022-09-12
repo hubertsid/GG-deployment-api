883f8248b5985bdc8f4d66af26e7af80460f1be7
Hubert Sidorowicz <80327910+hubertsid@users.noreply.github.com>
2022-09-12 22:43:57 +0200

    update

--- temp.py
+++ temp.py
@@ -2,7 +2,9 @@ import sqlite3
 
 conn = sqlite3.connect("game.db")
 c = conn.cursor()
-results = c.fetchall()
 
-for res in results[0][3:7]:
+query = "SELECT * FROM games"
+c.execute(query)
+results = c.fetchall()
+for res in results[0][1:3]:
     print(res)
